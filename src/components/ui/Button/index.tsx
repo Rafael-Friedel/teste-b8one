065858a08e58900eda46c04e0c392fbfbf0ff91a
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 12:47:04 -0300

    feat: create Button components

---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,37 @@
+import { VariantProps, cva, cx } from 'class-variance-authority';
+import React from "react";
+import checkIcon from "../../../assets/check.svg";
+
+const buttonStyles = cva('py-3 px-6 rounded-[5px] justify-center flex gap-3 uppercase font-bold text-base tracking-[-0.08px] w-full', {
+  variants: {
+    selected: {
+      true: "text-gray-900 bg-green-100",
+      false: "text-white bg-green-400 hover:bg-green-600"
+    }
+  }
+})
+
+interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  VariantProps<typeof buttonStyles> {
+  text: string
+  selected: boolean
+}
+
+const Button: React.FC<IButton> = ({ text, selected, ...props }) => {
+  return (
+    <button className={cx(buttonStyles({ selected }))} {...props}>
+      {selected && (
+        <img
+          src={checkIcon}
+          alt="icone de selecionado"
+          width={24}
+          height={24}
+          loading="lazy"
+        />
+      )}
+      <span>{text}</span>
+    </button>
+  )
+}
+
+export default Button
