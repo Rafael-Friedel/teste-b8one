37512fdce50a005e64abb9c0dc58a602fd21ec75
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 12:03:15 -0300

    chore: install and apply font to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className='App text-red-700 flex w-full h-screen justify-center items-center'>
-      Começando
+    <div className='App text-red-700 flex w-full h-screen justify-center items-center [&_*]:font-inter'>
+      <p>Começando</p>
     </div>
   )
 }
