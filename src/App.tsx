d1d3ff65ad68737ea0e3aa333d2ebcdc7cc49970
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 14:52:07 -0300

    feat: adjust css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { products } from "./mocks/data"
 
 function App() {
   return (
-    <div className='App flex w-full h-screen justify-center items-center [&_*]:font-inter bg-slate-100 gap-10'>
+    <div className='App flex flex-col lg:flex-row w-full h-full py-10 justify-center items-center [&_*]:font-inter bg-slate-100 gap-10'>
       {products.map((product, index) => (
         <CardProduct {...product} key={product.id + index} />
       ))}
