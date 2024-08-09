901a6fc9155b908eab8e2b3d8a28ba3c22eed758
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 12:13:45 -0300

    chore: defines the colors of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ export default {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        green: {
+          100: '#A3F9B9',
+          400: '#40B25C',
+          600: '#1C6C3E',
+        },
+        pink: {
+          100: '#FEE5EC',
+        },
+        red: {
+          500: '#DA4B4F',
+          600: '#C22539',
+        },
+        grayscale: {
+          200: '#F2F3F6',
+          600: '#B5B5B6',
+          900: '#1C1C1C',
+        },
+      },
     },
   },
   plugins: [],
