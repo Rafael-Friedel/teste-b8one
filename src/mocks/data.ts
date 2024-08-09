dd994be703764d60f5f5f2db6d0918ba0559ed12
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 14:34:09 -0300

    chore: create data mock

---
+++ src/mocks/data.ts
@@ -0,0 +1,22 @@
+import mockImageProduct from '../assets/image.svg';
+
+export const products = [
+  {
+    id: 1,
+    oldPrice: 1813.99,
+    currentPrice: 1599.0,
+    installments: 8,
+    productImage: mockImageProduct,
+    productName:
+      'Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 120 Hz HDMI, DP, Gsync Série CRG50',
+  },
+  {
+    id: 2,
+    oldPrice: 2813.99,
+    currentPrice: 2599.0,
+    installments: 10,
+    productImage: mockImageProduct,
+    productName:
+      'Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50',
+  },
+];
