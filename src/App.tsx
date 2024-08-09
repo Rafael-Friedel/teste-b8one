d574d75e763298aa376a549f1c61af3fa0d1f68f
Rafael Friedel <rafafriedeldev@outlook.com>
2024-08-09 14:39:11 -0300

    feat: render products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import CardProduct from "./components/CardProduct"
+import { products } from "./mocks/data"
+
 function App() {
   return (
-    <div className='App text-red-700 flex w-full h-screen justify-center items-center [&_*]:font-inter'>
-      <p>Começando</p>
+    <div className='App flex w-full h-screen justify-center items-center [&_*]:font-inter bg-slate-100 gap-10'>
+      {products.map((product, index) => (
+        <CardProduct {...product} key={product.id + index} />
+      ))}
     </div>
   )
 }
